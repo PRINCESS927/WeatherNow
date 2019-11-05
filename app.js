c62634a860d9c9b84ad859882db381a9d5a59a5c
PRINCESS <samisamare@gmail.com>
2019-11-05 15:56:13 -0500

    fin using open weather map api

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require("request");
 
 const app = express();
+
+const apiKey = process.env.APIKEY;
 
 //setting veiw engine
 app.set("veiw engine", "ejs");
@@ -17,14 +21,31 @@ app.use(bodyParser.urlencoded({extended: false}));
  //GET /S
 app.get('/', function (req, res) {
   let weathernow = "The weather in" + req.body.city + "is 70 C"
-    res.render("home.ejs", {weather:weathernow, error = err});
-    let err = false
-    
+  let err = false;
+  res.render("home.ejs", {weather: null, error:  null});
 });
 
+
 app.post("/", function(req, res){
-  let weathernow = "The weather in" + req.body.city + "is 70 C"
-    res.end();
+  let url = "http://api.openweathermap.org/data/2.5/weather?q=" + req.body.city + "London&units=imperial&APPID="+apiKey;
+
+
+    request(url, function (error, response, body) {
+     
+      if (error){
+        res.render("home.ejs", {weather: null, error: "error, please try again"})
+      } else{
+        let weather = JSON.parse(body);
+        if(weather.main == undefined){
+          res.render("home.ejs", {weather: null, error: "error, please try again"})
+      } else {
+        let weathernow = "The weather in" + req.body.city + " is " +weather.main.temp + "degrees.";
+        res.render("home.ejs", {weather: weathernow, error:  err});
+      }
+
+    
+    };
+  
 });
  
 app.listen(3000, function(){
